0c7a9289a4beec71c284dfb5145a3a810fdafdd2
atsta <atstamatiou@gmail.com>
2021-04-16 21:52:43 +0300

    first complete attempt

--- graph_coloring.py
+++ graph_coloring.py
@@ -27,11 +27,11 @@ edges = spark.createDataFrame([('1', '2'),
                                ('4', '3')],
                               ['src', 'dst'])
 
-superstep = 0
+#superstep = 0
 
 vertices = vertices.withColumn('color',lit(-1))
-vertices = vertices.withColumn('maxima',lit('true'))
-vertices = vertices.withColumn('superstep',lit(superstep))
+#vertices = vertices.withColumn('maxima',lit('true'))
+#vertices = vertices.withColumn('superstep',lit(superstep))
 
 superstep = 1
 
@@ -48,21 +48,46 @@ def get_max(a, b):
 get_max_udf = F.udf(get_max, types.IntegerType())
 
 def get_max_id(id, msg_ids, superstep):
+    #print(msg_ids)
+    print(superstep)
+    if len(msg_ids) == 0: 
+        return superstep
     return superstep if(int(max(msg_ids)) < int(id)) else -1
 get_max_id_udf = F.udf(get_max_id, types.IntegerType())
 
-max_iterations = 2
+max_iterations = 4
 
 
 for _ in range(max_iterations):
-    remaining = g.filterVertices("color == -1")
-    aggregates = remaining.aggregateMessages(F.collect_set(AM.msg).alias("agg"), sendToDst=AM.src["id"])
+    sub_g = g.filterVertices("color == -1")
+    
+    if (sub_g.vertices.count() == 1): 
+        res = sub_g.vertices.withColumn("newColor", lit(superstep)).drop("color")
+
+        new_vertices = g.vertices.join(res, on="id", how="left_outer") \
+                    .withColumnRenamed("color", "oldColor") \
+                    .withColumn("color", get_max_udf(F.col("oldColor"), F.col("newColor"))) \
+                    .drop("oldColor").drop("newColor")
+        
+        cached_new_vertices = AM.getCachedDataFrame(new_vertices)
+        g = GraphFrame(cached_new_vertices, g.edges)
+        g.vertices.show()
+
+        break
+
+    aggregates = sub_g.aggregateMessages(F.collect_set(AM.msg).alias("agg"), sendToDst=AM.src["id"])
     res = aggregates.withColumn("newColor", get_max_id_udf("id", "agg", lit(superstep))).drop("agg")
     
-    new_vertices = g.vertices.join(res, on="id", how="left_outer").withColumnRenamed("color", "oldColor").withColumn("color", get_max_udf(F.col("oldColor"), F.col("newColor"))).drop("oldColor").drop("newColor")
+    new_vertices = g.vertices.join(res, on="id", how="left_outer") \
+                    .withColumnRenamed("color", "oldColor") \
+                    .withColumn("color", get_max_udf(F.col("oldColor"), F.col("newColor"))) \
+                    .drop("oldColor").drop("newColor")
     
     superstep = superstep + 1
 
     cached_new_vertices = AM.getCachedDataFrame(new_vertices)
     g = GraphFrame(cached_new_vertices, g.edges)
     g.vertices.show()
+
+
+
